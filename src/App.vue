2931d1bd08e8783e70a11cb1e5b4d73ae4ec397d
IgorShum <Shumeikoigor@yandex.ru>
2022-01-11 11:02:14 +0300

    +add relations model-inputs +add createPost

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>
+  <div class="app">
+    <form @submit.prevent>
+      <h4>Создание поста</h4>
+      <input v-bind:value="title"
+      @input="title = $event.target.value"
+      class="new-post-input" 
+      type="text" placeholder="Название">
+
+      <input v-bind:value="body"
+      @input="body = $event.target.value"
+      class="new-post-input" 
+      type="text" placeholder="Описание">
+
+      <button type="submit" 
+      class="btn btn-create-post btn-success" 
+      @click="createPost">Создать</button>
+    </form>
     <div class="post"  v-for="post in posts" :key="post.id">
       <div><strong>Название:</strong>{{ post.title }}</div>
       <div><strong>Описание:</strong>{{ post.body }}</div>
@@ -14,7 +30,20 @@ export default {
         { id: 1, title: 'Frist-title', body: 'First description'},
         { id: 2, title: 'Second-title', body:  'description text'},
         { id: 3, title: 'Thrid', body: 'Lorem ipsum'},
-      ]
+      ],
+      title: '', 
+      body: ''
+    }
+  },
+  methods: {
+    createPost(){
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body
+      }
+      this.posts.push(newPost);
+      this.title=this.body='';
     }
   }
   
@@ -31,6 +60,32 @@ export default {
   padding: 15 px;
   margin-top: 15px;
   border: 2px solid teal;
+}
+
+.new-post-input {
+  width: 100%;
+  height: 30px;
+  border: 1px solid teal;
+  padding: 10px 15 px;
+  margin-top: 15px;
+}
+
+.app {
+  padding: 20px;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+
+}
+.btn-create-post {
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
 }
   
 </style>
